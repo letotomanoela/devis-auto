bf76fb42b032511aadacf5f2d8e5f703b4d5cc54
Letoto Manoela <100037690+letotomanoela@users.noreply.github.com>
2024-11-20 21:52:24 +0300

    footer test

--- __test__/footer.test.js
+++ __test__/footer.test.js
@@ -7,4 +7,17 @@ describe("Footer component", () => {
     const footerText = screen.getByText(/SUIVEZ-NOUS SUR/i);
     expect(footerText).toBeInTheDocument();
   });
+  it("should render 5 social item", () => {
+    render(<Footer />);
+    const socialsAlt = [
+      "facebook",
+      "instagram",
+      "linkedin",
+      "twitter",
+      "youtube",
+    ];
+    socialsAlt.forEach((item) => {
+      expect(screen.getByAltText(item)).toBeInTheDocument();
+    });
+  });
 });
