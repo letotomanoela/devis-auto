eb631b361146acc0d8108ee9bad53919b05e3a60
Letoto Manoela <100037690+letotomanoela@users.noreply.github.com>
2024-11-20 18:18:20 +0300

    changing into grid

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import Footer from "@/components/Footer";
 const Page = () => {
   return (
-    <div className="w-full h-screen grid grid-rows-[1fr_auto]">
+    <div className="grid h-screen w-full grid-rows-[1fr_auto]">
       <div></div>
       <Footer />
     </div>
